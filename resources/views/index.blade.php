220e72c2e9b6c7eafe1fe8b8b3ff44a269b44992
Eric Junior <eric.faria2003@gmail.com>
2024-07-27 01:45:57 -0300

    Alterado Index e CSS - Colocado card & footer, criado icones

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Barbearia</a>
+            <img class="logo navbar-brand" src="img/Logonbk.png" alt="Barbearia RD">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -22,7 +22,7 @@
                         <a class="nav-link" href="#services">Serviços</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#testimonials">Depoimentos</a>
+                        <a class="nav-link" href="#testimonials">Agendar</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#contact">Contato</a>
@@ -35,7 +35,7 @@
     <!-- Banner -->
     <div class="banner d-flex justify-content-center align-items-center">
         <div class="banner-text text-center">
-            <h1>Bem-vindo à Barbearia</h1>
+            <h1>Bem-vindo à Barbearia RD</h1>
             <p>Onde estilo encontra tradição</p>
         </div>
     </div>
@@ -45,25 +45,19 @@
         <div class="container">
             <div class="row text-center">
                 <div class="col-lg-4">
-                    <div class="service-icon mb-3">
-                        <i class="bi bi-scissors"></i>
-                    </div>
                     <h3>Corte de Cabelo</h3>
-                    <p>Profissionais especializados em diversos estilos de corte.</p>
+                    <p>Especializados em diversos estilos de corte.</p>
+                    <img class="imagecard" src="img/Corte.jpg" alt="">
                 </div>
                 <div class="col-lg-4">
-                    <div class="service-icon mb-3">
-                        <i class="bi bi-emoji-smile"></i>
-                    </div>
                     <h3>Barba</h3>
                     <p>Tratamento completo para uma barba impecável.</p>
+                    <img class="imagecard" src="img/teste1.jpg" alt="">
                 </div>
                 <div class="col-lg-4">
-                    <div class="service-icon mb-3">
-                        <i class="bi bi-brush"></i>
-                    </div>
-                    <h3>Tratamentos</h3>
-                    <p>Cuidados especiais para o seu cabelo e couro cabeludo.</p>
+                    <h3>Sombrancelha</h3>
+                    <p>Cuidados especiais para sua sombrancelha.</p>
+                    <img class="imagecard" src="img/teste2.jpg" alt="">
                 </div>
             </div>
         </div>
@@ -106,8 +100,8 @@
                             <input type="text" class="form-control" id="name" placeholder="Seu Nome">
                         </div>
                         <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" placeholder="Seu Email">
+                            <label for="telefone" class="form-label">Telefone</label>
+                            <input type="telefone" class="form-control" id="telefone" placeholder="(19) 99999-9999">
                         </div>
                         <div class="mb-3">
                             <label for="message" class="form-label">Mensagem</label>
@@ -119,11 +113,24 @@
                 <div class="col-lg-6">
                     <h2>Localização</h2>
                     <p>Rua Exemplo, 123, Centro, Cidade, Estado</p>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31109.846624049694!2d-74.0059416!3d40.7127757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzBCMDE0JzU2LjAiTiA3NMKwMDBCJzM0LjAiVw!5e0!3m2!1spt-BR!2sbr!4v1620076762018!5m2!1spt-BR!2sbr" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.200676005079!2d-46.9504253!3d-22.4320567!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8f972056a6afd%3A0x82421a75176182ba!2sRd%20Studio%20de%20Beleza%20e%20Barbearia!5e0!3m2!1spt-BR!2sbr!4v[tempo]
+" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                 </div>
             </div>
         </div>
     </section>
+
+    <footer>
+        <div class="footer-content">
+        <p>&copy; 2024 Barbearia RD. Todos os direitos reservados.</p>
+            <ul class="socials">
+             <li><a href="https://www.facebook.com/arrazusrd/?locale=pt_BR"><img src="https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg" alt="Facebook"></a></li>
+             <li><a href="#"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAUVBMVEVHcEzz9PP3+Pfq7er19vb//v/8/PyltKrx+PKz5b1z04cwxlYMwUMAvCkAvjMAvzoDwUBGTUiH2JiY3aXX8Nxcznbo6ulJymfQ1NHJzcq9wr8gaKU6AAAAG3RSTlMAOlmJ2f//Lvj+/v///////xX+/v7+rP56aF7BK/qjAAABgklEQVR4AX2Sh5qEMAiEbYyJqXa993/QAza5z2v7b8UMMKDNN9qu77u2+Yd+oMLQ/3UMeoCfktHIVTs5791kRWvGb73BV6YQU2ZSDJPo28c5hzYk/0UKlghfipnzl+C/ERZWVMHA59H7nL8pVp6mUTqp7310Ln1TbESdCtgQJ0fO2J+KnNl4cbhGnx0b3aJ/EO1rkoPIZZ9WAuCeNhLnHGrRBJUDtH53YdQmyEaWL1Lh+yCSxEsoeQn4btJr27kZQYtclym279uSqqMI1lTntqpI6VlhVg9M9gArsg92Cal6mHlPdfx8Emg75S6YV9dNN3WAaskTsi0Zh6bEMUH2cLHLqvDsA4LsTjzeerNhqvscJpYwa5Q9gS0w/XODKbjF2jPK3PXJrIMWcooxiyGSAkKvHX+QHICPRhnYQ0mr5LADpYFuShunWIeJ2VI5Z25gM8TA7jFEfu9Whr2awoEKi8ymYmAYm4oBh8fHfA8vESRme5URxz0XN9cxMMelcYWDt3wCfLshX3540l4AAAAASUVORK5CYII=" alt="WhatsApp"></a></li>
+             <li><a href="https://www.instagram.com/daniel_rdbarbearia/"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/768px-Instagram_icon.png" alt="Instagram"></a></li>
+            </ul>
+        </div>
+    </footer>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
